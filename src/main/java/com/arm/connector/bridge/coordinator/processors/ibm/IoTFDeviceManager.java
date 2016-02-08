84362fcde07e663073330fbad88f1b11a3828436
Doug Anson <doug.anson@arm.com>
2016-02-08 17:25:16 -0600

    renamed iotf creds to better align with the IBM pages

--- src/main/java/com/arm/connector/bridge/coordinator/processors/ibm/IoTFDeviceManager.java
+++ src/main/java/com/arm/connector/bridge/coordinator/processors/ibm/IoTFDeviceManager.java
@@ -44,14 +44,14 @@ public class IoTFDeviceManager extends BaseClass {
     private String m_iotf_gw_auth_token = null;
     
     private String m_iotf_org_id = null;
-    private String m_iotf_api_key = null;
-    private String m_iotf_password = null;
+    private String m_iotf_org_key = null;
+    private String m_iotf_auth_token = null;
     
-    private String m_iotf_username = null;
+    private String m_iotf_api_key = null;
     private String m_iotf_gw_client_id = null;
     
-    private String m_gw_iotf_username = null;
-    private String m_gw_iotf_password = null;
+    private String m_iotf_gw_key = null;
+    private String m_gw_iotf_auth_token = null;
        
     private String m_suffix = null;
     
@@ -74,7 +74,7 @@ public class IoTFDeviceManager extends BaseClass {
         this.m_iotf_gw_dev_type_id = this.preferences().valueOf("iotf_gw_dev_type_id",this.m_suffix);
         
         this.m_iotf_org_id = this.preferences().valueOf("iotf_org_id",this.m_suffix);
-        this.m_iotf_api_key = this.preferences().valueOf("iotf_api_key",this.m_suffix);
+        this.m_iotf_org_key = this.preferences().valueOf("iotf_org_key",this.m_suffix);
        
         this.m_iotf_rest_uri_template = this.preferences().valueOf("iotf_rest_uri_template",this.m_suffix);
         this.m_iotf_add_gw_type_template =  this.preferences().valueOf("iotf_add_gw_type_template",this.m_suffix);
@@ -84,20 +84,20 @@ public class IoTFDeviceManager extends BaseClass {
         this.m_iotf_rest_hostname = this.preferences().valueOf("iotf_rest_hostname_template",this.m_suffix).replace("__ORG_ID__",this.m_iotf_org_id);
         
         // build out Basic Auth username/password for the REST calls
-        this.m_iotf_username = "a-" + this.m_iotf_org_id + "-" + this.m_iotf_api_key;
-        this.m_gw_iotf_username = "g-" + this.m_iotf_org_id + "-" + this.m_iotf_gw_type_id + "-" + this.m_iotf_gw_id;
-        this.m_iotf_password = this.preferences().valueOf("iotf_password",this.m_suffix);
-        this.m_iotf_gw_auth_token = Utils.createURLSafeToken(this.m_iotf_password);
+        this.m_iotf_api_key = "a-" + this.m_iotf_org_id + "-" + this.m_iotf_org_key;
+        this.m_iotf_gw_key = "g-" + this.m_iotf_org_id + "-" + this.m_iotf_gw_type_id + "-" + this.m_iotf_gw_id;
+        this.m_iotf_auth_token = this.preferences().valueOf("iotf_auth_token",this.m_suffix);
+        this.m_iotf_gw_auth_token = Utils.createURLSafeToken(this.m_iotf_auth_token);
     }
     
     // update the OrgID and APIKey
     public void updateIoTFBindings(String org_id,String api_key) {
         this.m_iotf_org_id = org_id;
-        this.m_iotf_api_key = api_key;
+        this.m_iotf_org_key = api_key;
         this.m_iotf_rest_hostname = this.preferences().valueOf("iotf_rest_hostname_template",this.m_suffix).replace("__ORG_ID__",this.m_iotf_org_id);
-        this.m_iotf_username = "a-" + this.m_iotf_org_id + "-" + this.m_iotf_api_key;
-        this.m_gw_iotf_username = "g-" + this.m_iotf_org_id + "-" + this.m_iotf_gw_type_id + "-" + this.m_iotf_gw_id;
-        this.m_gw_iotf_password = this.m_iotf_gw_auth_token;
+        this.m_iotf_api_key = "a-" + this.m_iotf_org_id + "-" + this.m_iotf_org_key;
+        this.m_iotf_gw_key = "g-" + this.m_iotf_org_id + "-" + this.m_iotf_gw_type_id + "-" + this.m_iotf_gw_id;
+        this.m_gw_iotf_auth_token = this.m_iotf_gw_auth_token;
         this.initIoTFMetadata();
     }
     
@@ -185,7 +185,7 @@ public class IoTFDeviceManager extends BaseClass {
         String payload = this.createAddGatewayTypeJSON();
         
         // DEBUG
-        //this.errorLogger().info("installGatewayType: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_username + " PW: " + this.m_iotf_password);
+        //this.errorLogger().info("installGatewayType: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_api_key + " PW: " + this.m_iotf_auth_token);
         
         // dispatch and look for the result
         String result = this.post(url, payload);
@@ -206,7 +206,7 @@ public class IoTFDeviceManager extends BaseClass {
         String payload = this.createAddGatewayDeviceTypeJSON();
         
         // DEBUG
-        //this.errorLogger().info("installGatewayDeviceType: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_username + " PW: " + this.m_iotf_password);
+        //this.errorLogger().info("installGatewayDeviceType: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_api_key + " PW: " + this.m_iotf_auth_token);
         
         // dispatch and look for the result
         String result = this.post(url, payload);
@@ -230,7 +230,7 @@ public class IoTFDeviceManager extends BaseClass {
         String payload = this.createAddGatewayJSON();
         
         // DEBUG
-        //this.errorLogger().info("installGatewayDeviceType: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_username + " PW: " + this.m_iotf_password);
+        //this.errorLogger().info("installGatewayDeviceType: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_api_key + " PW: " + this.m_iotf_auth_token);
         
         // dispatch and look for the result
         String result = this.post(url, payload);
@@ -264,21 +264,21 @@ public class IoTFDeviceManager extends BaseClass {
     
     // GET specific URL contents
     private String get(String url) {
-        String result = this.m_http.httpGet(url, this.m_iotf_username, this.m_iotf_password, null, "application/json", null);
+        String result = this.m_http.httpGet(url, this.m_iotf_api_key, this.m_iotf_auth_token, null, "application/json", null);
         //this.errorLogger().info("get: URL: " + url + " RESULT: " + result);
         return result;
     }
     
     // POST specific data to a given URL
     private String post(String url,String payload) {
-        String result = this.m_http.httpsPost(url, this.m_iotf_username, this.m_iotf_password, payload, "application/json", null);
+        String result = this.m_http.httpsPost(url, this.m_iotf_api_key, this.m_iotf_auth_token, payload, "application/json", null);
         //this.errorLogger().info("post: URL: " + url + " DATA: " + payload + " RESULT: " + result);
         return result;
     }
     
     // POST specific data to a given URL
     private String gwpost(String url,String payload) {
-        String result = this.m_http.httpsPost(url, this.m_gw_iotf_username, this.m_gw_iotf_password, payload, "application/json", null);
+        String result = this.m_http.httpsPost(url, this.m_iotf_gw_key, this.m_gw_iotf_auth_token, payload, "application/json", null);
         //this.errorLogger().info("post: URL: " + url + " DATA: " + payload + " RESULT: " + result);
         return result;
     }
@@ -292,7 +292,7 @@ public class IoTFDeviceManager extends BaseClass {
     
     // DELETE specific data to a given URL (with data)
     private String delete(String url,String payload) {
-        return this.m_http.httpsDelete(url, this.m_iotf_username, this.m_iotf_password, payload, "application/json", null);
+        return this.m_http.httpsDelete(url, this.m_iotf_api_key, this.m_iotf_auth_token, payload, "application/json", null);
     }
     
     // DELETE specific data to a given URL
@@ -304,7 +304,7 @@ public class IoTFDeviceManager extends BaseClass {
     
     // DELETE specific data to a given URL (with data)
     private String gwdelete(String url,String payload) {
-        return this.m_http.httpsDelete(url, this.m_gw_iotf_username, this.m_gw_iotf_password, payload, "application/json", null);
+        return this.m_http.httpsDelete(url, this.m_iotf_gw_key, this.m_gw_iotf_auth_token, payload, "application/json", null);
     }
     
     // build out the metadata JSON
@@ -439,7 +439,7 @@ public class IoTFDeviceManager extends BaseClass {
         String payload = this.createAddDeviceJSON(message);
         
         // DEBUG
-        //this.errorLogger().info("registerNewDevice: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_username + " PW: " + this.m_iotf_password);
+        //this.errorLogger().info("registerNewDevice: URL: " + url + " DATA: " + payload + " USER: " + this.m_iotf_api_key + " PW: " + this.m_iotf_auth_token);
         
         // dispatch and look for the result
         String result = this.gwpost(url, payload);
@@ -460,7 +460,7 @@ public class IoTFDeviceManager extends BaseClass {
         url += "/devices/" + device;
         
         // DEBUG
-        //this.errorLogger().info("deregisterDevice: URL: " + url + " USER: " + this.m_iotf_username + " PW: " + this.m_iotf_password);
+        //this.errorLogger().info("deregisterDevice: URL: " + url + " USER: " + this.m_iotf_api_key + " PW: " + this.m_iotf_auth_token);
         
         // dispatch and look for the result
         String result = this.gwdelete(url);
