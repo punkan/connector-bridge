cba25ae8a444d0a107960bc2aa4e0a3611920b50
Doug Anson <doug.anson@arm.com>
2016-03-27 11:30:05 -0500

    set license to Apache 2.0

--- src/main/java/com/arm/connector/bridge/coordinator/Orchestrator.java
+++ src/main/java/com/arm/connector/bridge/coordinator/Orchestrator.java
@@ -7,10 +7,17 @@
  *
  * Copyright 2015. ARM Ltd. All rights reserved.
  *
- * Use of this software is restricted by the terms of the license under which this software has been
- * distributed (the "License"). Any use outside the express terms of the License, including wholly
- * unlicensed use, is prohibited. You may not use this software unless you have been expressly granted
- * the right to use it under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  */
 
@@ -25,7 +32,7 @@ import com.arm.connector.bridge.coordinator.processors.sample.Sample3rdPartyProc
 import com.arm.connector.bridge.core.ErrorLogger;
 import com.arm.connector.bridge.json.JSONGenerator;
 import com.arm.connector.bridge.json.JSONParser;
-import com.arm.connector.bridge.json.JsonGeneratorFactory;
+import com.arm.connector.bridge.json.JSONGeneratorFactory;
 import com.arm.connector.bridge.preferences.PreferenceManager;
 import com.arm.connector.bridge.transport.HttpTransport;
 import java.util.ArrayList;
@@ -55,7 +62,7 @@ public class Orchestrator implements MDSInterface, PeerInterface {
 
     private HttpTransport            m_http = null;
     
-    private JsonGeneratorFactory     m_json_factory = null;
+    private JSONGeneratorFactory     m_json_factory = null;
     private JSONGenerator            m_json_generator = null;
     private JSONParser               m_json_parser = null;
     private boolean                  m_listeners_initialized = false;
@@ -74,7 +81,7 @@ public class Orchestrator implements MDSInterface, PeerInterface {
         }
           
         // JSON Factory
-        this.m_json_factory = JsonGeneratorFactory.getInstance();
+        this.m_json_factory = JSONGeneratorFactory.getInstance();
         
         // create the JSON Generator
         this.m_json_generator = this.m_json_factory.newJsonGenerator();
